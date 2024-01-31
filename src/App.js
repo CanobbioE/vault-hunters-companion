dec7f340680c4cf57c7746aa8ed10e5a26647dfe
Edoardo Canobbio <canobbio.e@gmail.com>
2024-01-31 21:03:30 +0100

    keep screen awake on run screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 import StartScreen from './screens/StartScreen';
 import RunScreen from './screens/RunScreen';
+import NoSleep from 'nosleep.js';
 
 
 // todo: running, map, reset
@@ -9,17 +10,36 @@ import RunScreen from './screens/RunScreen';
 function App() {
   const [screen, setScreen] = useState(0)
   const [direction, setDirection] = useState(null)
+  const [enabled, setEnabled] = useState(false)
+  const [alreadyEnabled, setAlreadyEnabled] = useState(false);
 
   const startScreen = <StartScreen
     onStart={(direction) => {
       setScreen(1)
       setDirection(direction)
+      setEnabled(true)
     }} />
 
   const runScreen = (<RunScreen direction={direction} />)
 
 
   const screens = [startScreen,runScreen]
+
+  // keep the screen awake
+  const noSleep = useMemo(() => new NoSleep(), []);
+  useEffect(() => {
+      if (alreadyEnabled === enabled) {
+          return;
+      }
+
+      if (enabled) {
+          noSleep.enable();
+      } else {
+          noSleep.disable();
+      }
+
+      setAlreadyEnabled(enabled);
+  }, [alreadyEnabled, enabled, noSleep]);
 
   return (
     <div className='App'>
